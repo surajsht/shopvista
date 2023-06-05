b1b3a7398235e11dd173b034b2eb028acbcecbaf
suraj <surajsht2055@gmail.com>
2023-06-05 21:38:07 +0545

    hero section design complete

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import heroImage from "../../assets/hero-image.png";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <img src={heroImage} alt="" />
+
+      <div className="hero-content">
+        <h2 className="hero-title">Rest, Relax, Unwind</h2>
+        <h3 className="hero-sub-title"> Embrace your choices - we do </h3>
+        <button className="primary-btn">
+          <Link to="/product"> Shop Now </Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Navbar from "../component/navbar/Navbar";
 import Hero from "../component/hero/Hero";
 
 const Home = () => {
+  useEffect(() => {
+    document.body.classList.add("home");
+
+    return () => {
+      document.body.classList.remove("home");
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
