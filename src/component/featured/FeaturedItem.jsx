7211faf348ac64653cc2f8ede8b059ed667b4eca
suraj <surajsht2055@gmail.com>
2023-06-07 20:27:15 +0545

    feature title link redirected to single

--- src/component/featured/FeaturedItem.jsx
+++ src/component/featured/FeaturedItem.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
 const FeaturedItem = ({ data }) => {
-  let { image, title, price } = data;
+  let { id, image, title, price } = data;
   return (
     <div className="featured-item">
       <img src={image} alt="" />
       <h2 className="shopvista-item-title">
-        <Link to="/product"> {title} </Link>
+        <Link to={`/product/${id}`}> {title} </Link>
       </h2>
       <span className="shopvista-price"> ${price} </span>
     </div>
